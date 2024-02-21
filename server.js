c0b85d84df2338522b8451553ec90e0acbcac0fe
EasySimho <s.benanchietti.yt@gmail.com>
2024-02-21 18:19:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const express = require('express');
-const path = require('path'); // Importa il modulo path
+const path = require('path');
 
 const app = express();
 const port = 8080;
 
-// Serve i file statici dalla directory 'public'
 app.use(express.static('public'));
 
-// Route per la radice del server
 app.get('/', (req, res) => {
-  // Invia il file 'index.html' come risposta
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server listening at http://0.0.0.0:${port}`);
 });
